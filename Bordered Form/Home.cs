63b4065969ef865483a49292ccd13cfae42f7adf
Banee Ishaque K <Baneeishaque@gmail.com>
2018-12-07 16:59:27 +0530

    Add project files.

---
+++ Bordered Form/Home.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Bordered_Form
+{
+    public partial class Home : Form
+    {
+        public Home()
+        {
+            InitializeComponent();
+           
+        }
+
+        public Home(Point point)
+        {
+            InitializeComponent();
+
+            
+
+        }
+
+
+    }
+}
